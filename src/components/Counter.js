c2088b2d483b99279f9429027f1d5f510c126ee5
piotryfon <pit.pit@wp.pl>
2019-05-29 19:21:48 +0200

    addition and subtraction functions added in counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,12 +7,18 @@ class Counter extends React.Component {
             result: 0
         }
     }
+    addition = () => {
+        this.setState({result: this.state.result + 1})
+    }
+    subtraction = () => {
+        this.setState({result: this.state.result - 1})
+    }
     render() {
         return (
             <div>
-                <button class="btn btn-danger">+</button>
+                <button class="btn btn-danger" onClick={this.addition}> + </button>
                 <h2>{this.state.result}</h2>
-                <button class="btn btn-primary">-</button>
+                <button class="btn btn-primary" onClick={this.subtraction}> - </button>
             </div>
         )
     }
